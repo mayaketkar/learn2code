91afb615e64ed7162443e28ae8c8106d67de3db4
Maya Ketkar <maya@Nikhils-MacBook-Pro.local>
2024-12-29 20:40:45 +0530

    Adding files from Dad's machine

---
+++ new/excercise13.py
@@ -0,0 +1,10 @@
+from sys import argv 
+
+
+print("Start")
+script, first, second, third = argv
+
+print("The first is called:", script)
+print("Your first variabal is:", first)
+print("Your second variable is", second)
+print("Your third variable is", third)
---
+++ new/test.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+def print_maya():
+    print("Maya")
+    print("Nikhil")
+    print("Luna")
+    print("Aditi")
+
+def add_2_numbers(x, y):
+    print(x + y)
+
+if __name__ == "__main__":
+    add_2_numbers(1, 2)
