fa0185a89dabd06d17c4a150199348ac34a89097
Maya Ketkar <maya.n.ketkar@gmail.com>
2024-12-30 19:03:14 +0530

    exercise 23 from book

--- new/exercise23.py
+++ new/exercise23.py
@@ -9,7 +9,7 @@ def main(language_file, encoding, errors):
         return main(language_file, encoding, errors)
 
 
-def print_line(line, encode, errors):
+def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors) 
     cooked_string = raw_bytes.decode(encoding, errors=errors)
@@ -17,7 +17,7 @@ def print_line(line, encode, errors):
 
     print(raw_bytes, "<===>", cooked_string)
 
-languages = open("languages.txt", encoding="utf-8") 
+languages = open("/Users/maya/Downloads/languages.txt", encoding="utf-8") 
 
 main(languages, input_encoding, error)
 
