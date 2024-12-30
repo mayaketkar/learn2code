6be6f005c54fce381b5c7237b989f0a570f536b7
Maya Ketkar <maya.n.ketkar@gmail.com>
2024-12-30 16:30:11 +0530

    new code

--- new/exercise21.py
+++ new/exercise21.py
@@ -1,15 +1,15 @@
 def add (a, b):
-    print(f"ADDING {a} + {b}")
+    print(f"ADDING {a} + {b} This will be my age")
     return a + b
 
 def subtract (a, b):
-    print(f"SUBTRACTING {a} - {b}")
+    print(f"SUBTRACTING {a} - {b} This will be my height")
     return a - b
 def multiply(a, b):
-    print(f"MULTIPLYING {a} * {b}")
+    print(f"MULTIPLYING {a} * {b} This will be my weight ")
     return a * b
 def divide(a, b):
-    print(f"DIVIDING {a} / {b}")
+    print(f"DIVIDING {a} / {b} This is my iq!!!")
     return a / b
 print("Let's do some math with just functions!") 
 age = add(30, 5)
@@ -23,5 +23,5 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 print("Here is a puzzle.")
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
-
+print("That becomes: ", what, "Can you do it by hand?")
 
